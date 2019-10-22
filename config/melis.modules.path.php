bf894328380d74daf119fe0eaa5e440e6beeff88
Ben Borla <benborla@icloud.com>
2019-10-22 23:20:24 +0800

    added db

---
+++ config/melis.modules.path.php
@@ -0,0 +1,13 @@
+<?php 
+
+	return array( 
+		'GraphQLPlayground' => '/module/GraphQLPlayground', 
+		'MelisModuleConfig' => '/module/MelisModuleConfig', 
+		'MelisSites' => '/module/MelisSites', 
+		'MelisAssetManager' => '/vendor/melisplatform/melis-asset-manager', 
+		'MelisComposerDeploy' => '/vendor/melisplatform/melis-composerdeploy', 
+		'MelisCore' => '/vendor/melisplatform/melis-core', 
+		'MelisDbDeploy' => '/vendor/melisplatform/melis-dbdeploy', 
+		'MelisInstaller' => '/vendor/melisplatform/melis-installer', 
+		'MelisMarketPlace' => '/vendor/melisplatform/melis-marketplace', 
+	); 
